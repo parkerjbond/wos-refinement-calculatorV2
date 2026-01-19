5361b30002f002a6f8023ac981a77f8a459d1843
parkerjbond <pbomb04@gmail.com>
2026-01-19 11:26:19 -0700

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -3,4 +3,10 @@ const childOne = document.querySelector('.child-one');
 
 btn.addEventListener('click', () => {
   childOne.classList.toggle('is-expanded');
+
+  if (childOne.classList.contains('is-expanded')) {
+    btn.innerHTML = '<h3>Recline Tower Details</h3>';
+  } else {
+    btn.innerHTML = '<h3>Expand Tower Details</h3>';
+  }
 });
